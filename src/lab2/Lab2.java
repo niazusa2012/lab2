f3aaf942b07d208ac21616fbfa5084ea3a82660c
class <class@CST_N417_10.cst.citytech.cuny.edu>
2015-03-02 16:28:56 -0500

    starting lab 2

---
+++ src/lab2/Lab2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author class
+ */
+public class Lab2 {
+
+    
+    public static void main(String[] args) {
+        int total,quarter ;
+        quarter =  total/25;
+        System.out.println("Please enter your amount: " );
+        Scanner.Keyboard = new Scanner(System.in);
+        total = Keyboard.NextInt();
+    }
+    
+}
